7f95e0ad95fc1c9f2df31722ad3cf46f57536feb
saeed <saeednoferest@gmail.com>
2025-04-14 19:27:20 +0330

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("salam");
         System.out.println("Hello, World!");
     }
 }
