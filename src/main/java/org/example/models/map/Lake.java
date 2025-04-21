58a33651b7d61523c6d1b3fb0898ee1e41c9a2e8
saeed <saeednoferest@gmail.com>
2025-04-21 21:55:27 +0330

    Save untracked changes before switching branch

---
+++ src/main/java/org/example/models/map/Lake.java
@@ -0,0 +1,6 @@
+package org.example.models.map;
+
+import org.example.models.Placeable;
+
+public class Lake implements Placeable {
+}
---
+++ src/main/java/org/example/models/trade/ShippingBin.java
@@ -0,0 +1,7 @@
+package org.example.models.trade;
+import org.example.models.enums.ShippingBinType;
+
+public class ShippingBin {
+    private ShippingBinType type;
+
+}
