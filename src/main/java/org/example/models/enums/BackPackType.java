3ba70e0ba0c346a7f080da0e8c52fa9e2b0201e6
saeed <saeednoferest@gmail.com>
2025-04-21 20:26:23 +0330

    test

--- src/main/java/org/example/models/enums/BackPackType.java
+++ src/main/java/org/example/models/enums/BackPackType.java
@@ -2,8 +2,8 @@ package org.example.models.enums;
 
 public enum BackPackType {
     PrimaryBackpack(12),
-    BigBackPack(24),  //TODO: available in Pierre Store
-    DeluxeBackPack((int)Double.POSITIVE_INFINITY);  //TODO: available in Pierre Store after we have purchased a BigBackPack
+    BigBackPack(24), //TODO: available in Pierre Store
+    DeluxeBackPack((int)Double.POSITIVE_INFINITY); //TODO: available in Pierre Store after we have purchased a BigBackPack
 
     private final int capacity;
 
