21021b9d095bac863bd2c57cbf1831d4bbdc7104
saeed <saeednoferest@gmail.com>
2025-05-01 20:06:28 +0330

    1404,2,9 fixed hug and talk (saeednoferesti)

--- src/main/java/org/example/models/Gift.java
+++ src/main/java/org/example/models/Gift.java
@@ -1,6 +1,40 @@
 package org.example.models;
 
 public class Gift {
+    private Player playerWhoGiveGift;
+    private Player playerWhoGetGift;
     private int giftNumber;
     private Product product;
+
+    public Player getPlayerWhoGiveGift() {
+        return playerWhoGiveGift;
+    }
+
+    public void setPlayerWhoGiveGift(Player playerWhoGiveGift) {
+        this.playerWhoGiveGift = playerWhoGiveGift;
+    }
+
+    public Player getPlayerWhoGetGift() {
+        return playerWhoGetGift;
+    }
+
+    public void setPlayerWhoGetGift(Player playerWhoGetGift) {
+        this.playerWhoGetGift = playerWhoGetGift;
+    }
+
+    public int getGiftNumber() {
+        return giftNumber;
+    }
+
+    public void setGiftNumber(int giftNumber) {
+        this.giftNumber = giftNumber;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
