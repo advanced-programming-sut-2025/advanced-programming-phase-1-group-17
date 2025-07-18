ef0be1f5a55a800be32b199148363d742a62342e
saeed <saeednoferest@gmail.com>
2025-07-18 22:56:19 +0330

    Flower and askMarriage

--- core/src/main/java/io/github/StardewValley/views/GameView.java
+++ core/src/main/java/io/github/StardewValley/views/GameView.java
@@ -18,6 +18,7 @@ import io.github.StardewValley.models.Player;
 import io.github.StardewValley.models.Result;
 import io.github.StardewValley.models.enums.Gender;
 import io.github.StardewValley.models.market.StoreType;
+
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -27,7 +28,7 @@ public class GameView implements Screen, InputProcessor {
     private final GameMenuController menuController;
     private HUD hud;
     private Window window;
-    private TextButton talkButton, tradeButton, hugButton, giftButton, askMarriageButton;
+    private TextButton talkButton, tradeButton, hugButton, giftButton, givingFlower, askMarriageButton;
     private Label error;
     private Table content;
     private boolean activeWindow = true;
@@ -51,6 +52,7 @@ public class GameView implements Screen, InputProcessor {
         tradeButton = new TextButton("Trade", skin);
         hugButton = new TextButton("Hug", skin);
         giftButton = new TextButton("Gift", skin);
+        givingFlower = new TextButton("Giving Flower", skin);
         askMarriageButton = new TextButton("Ask Marriage", skin);
         error = new Label("", skin);
         content = new Table(skin);
@@ -59,6 +61,7 @@ public class GameView implements Screen, InputProcessor {
         window.add(tradeButton).width(60).height(30).pad(10).row();
         window.add(giftButton).width(60).height(30).pad(10).row();
         window.add(hugButton).width(60).height(30).pad(10).row();
+        window.add(givingFlower).width(60).height(30).pad(10).row();
         window.add(askMarriageButton).width(60).height(30).pad(10).row();
     }
 
@@ -88,7 +91,6 @@ public class GameView implements Screen, InputProcessor {
                         if (r.isSuccessful()) {
                             Gender playerGender = App.getCurrentGame().getCurrentPlayingPlayer().getUser().getGender();
                             Gender targetGender = currentTargetPlayer.getUser().getGender();
-                            window.setVisible(false);
                             String imagePath;
                             if (playerGender == targetGender) {
                                 imagePath = (targetGender == Gender.Male) ? "hug1.png" : "hug3.png";
@@ -121,8 +123,47 @@ public class GameView implements Screen, InputProcessor {
             public void clicked(InputEvent event, float x, float y) {
                 if (currentTargetPlayer == null) return;
                 Main.getMain().getScreen().dispose();
-                Main.getMain().setScreen(new GiftMenu(App.getCurrentGame().getCurrentPlayingPlayer(),new GiftMenuController(),
-                    GameAssetManager.getGameAssetManager().getSkin(), currentTargetPlayer,gameView));
+                Main.getMain().setScreen(new GiftMenu(App.getCurrentGame().getCurrentPlayingPlayer(), new GiftMenuController(),
+                    GameAssetManager.getGameAssetManager().getSkin(), currentTargetPlayer, gameView,null));
+            }
+        });
+        givingFlower.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                if (currentTargetPlayer == null) return;
+                Result r = menuController.flower(currentTargetPlayer.getUser().getUsername());
+                error.setText(r.toString());
+                com.badlogic.gdx.utils.Timer.schedule(new com.badlogic.gdx.utils.Timer.Task() {
+                    @Override
+                    public void run() {
+                        if (r.isSuccessful()) {
+                            Image image = new Image(new Texture(Gdx.files.internal("Flower.jpg")));
+                            image.setSize(500, 500);
+                            image.setPosition(stage.getWidth() / 2f - image.getWidth() / 2f,
+                                stage.getHeight() / 2f - image.getHeight() / 2f);
+
+                            window.setVisible(false);
+                            stage.addActor(image);
+
+                            com.badlogic.gdx.utils.Timer.schedule(new com.badlogic.gdx.utils.Timer.Task() {
+                                @Override
+                                public void run() {
+                                    image.remove();
+                                    window.setVisible(true);
+                                }
+                            }, 3);
+
+                        }
+                    }
+                }, 2);
+
+
+            }
+        });
+        askMarriageButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                if (currentTargetPlayer == null) return;
+                Result r = menuController.askMarriage(currentTargetPlayer.getUser().getUsername(),"Ring");
+                error.setText(r.toString());
             }
         });
     }
@@ -203,7 +244,7 @@ public class GameView implements Screen, InputProcessor {
 
     private void showInteractionWindow(Player targetPlayer) {
         Vector3 screenPos = controller.getCamera().project(new Vector3(targetPlayer.getX(), targetPlayer.getY(), 0));
-        window.setSize(250, 300);
+        window.setSize(250, 350);
         window.setPosition(screenPos.x - window.getWidth() / 2f, screenPos.y);
         window.setMovable(true);
         window.setResizable(false);
@@ -213,14 +254,33 @@ public class GameView implements Screen, InputProcessor {
         }
     }
 
-    @Override public void resize(int width, int height) {}
-    @Override public void pause() {}
-    @Override public void resume() {}
-    @Override public void hide() {}
-    @Override public void dispose() {}
+    @Override
+    public void resize(int width, int height) {
+    }
 
-    @Override public boolean keyTyped(char c) { return false; }
-    @Override public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         Vector3 worldCoordinates = controller.getCamera().unproject(new Vector3(screenX, screenY, 0));
         for (Map.Entry<StoreType, Rectangle> entry : controller.getStoreBounds().entrySet()) {
             if (entry.getValue().contains(worldCoordinates.x, worldCoordinates.y)) {
@@ -232,13 +292,44 @@ public class GameView implements Screen, InputProcessor {
         }
         return false;
     }
-    @Override public boolean touchUp(int x, int y, int pointer, int button) { return false; }
-    @Override public boolean touchCancelled(int x, int y, int pointer, int button) { return false; }
-    @Override public boolean touchDragged(int x, int y, int pointer) { return false; }
-    @Override public boolean mouseMoved(int x, int y) { return false; }
-    @Override public boolean scrolled(float amountX, float amountY) { return false; }
-    @Override public boolean keyDown(int keycode) { controller.setKey(keycode, true); return true; }
-    @Override public boolean keyUp(int keycode) { controller.setKey(keycode, false); return true; }
+
+    @Override
+    public boolean touchUp(int x, int y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int x, int y, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int x, int y, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int x, int y) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        controller.setKey(keycode, true);
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        controller.setKey(keycode, false);
+        return true;
+    }
+
     public void setError(String error) {
         this.error.setText(error);
     }
