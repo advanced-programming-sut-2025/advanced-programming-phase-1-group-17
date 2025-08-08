254a5c70ef2d2e0ef3f0360666b0632e8f90c0b1
Saeed <saeednoferesti4444@gmail.com>
2025-08-08 13:02:11 +0330

    fixed delete app

--- shared/src/main/java/io/github/StardewValley/shared/models/map/GameMap.java
+++ shared/src/main/java/io/github/StardewValley/shared/models/map/GameMap.java
@@ -1,5 +1,6 @@
 package io.github.StardewValley.shared.models.map;
 
+import io.github.StardewValley.shared.models.Game;
 import io.github.StardewValley.shared.models.enums.Gender;
 import io.github.StardewValley.shared.models.NPCS.NPC;
 import io.github.StardewValley.shared.models.Player;
@@ -10,14 +11,14 @@ import java.util.ArrayList;
 public class GameMap {
     private ArrayList<PlayerMap> playerMaps = new ArrayList<>();
 
-    public GameMap(ArrayList<Player> players) {
+    public GameMap(ArrayList<Player> players, Game game) {
         UserDTO user = new UserDTO("NPC",  "NPC", Gender.Male);
         NPC.setFatherUser(user);
         Player player = new Player(user, true);
         NPC.setFatherPlayer(player);
         players.add(player);
         for (int i = 0; i < 5; i++) {
-            playerMaps.add(new PlayerMap(i, players.get(i)));
+            playerMaps.add(new PlayerMap(i, players.get(i),game));
         }
     }
 
